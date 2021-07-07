1e0ec00a5a7e79d6b26cd69bc83034318e75a18f
Alejandro <alejo_540@hotmail.com>
2021-07-07 14:19:16 -0500

    commit

--- src/main/java/com/everis/topic/ProductTopic.java
+++ src/main/java/com/everis/topic/ProductTopic.java
@@ -46,7 +46,7 @@ public class ProductTopic {
   
 //    config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, hostName + ":" + port);
     
-    config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "20.88.211.57:80");
+    config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "20.88.211.57:9092");
   
     config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
   
